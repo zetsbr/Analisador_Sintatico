83ba4c94adba1cd120f455c9b319bc87b045310d
João Porto <jvaporto@gmail.com>
2021-03-11 18:40:28 -0300

    arrumei o erro do end.

--- Analisador_Sintatico.py
+++ Analisador_Sintatico.py
@@ -15,58 +15,63 @@ txt=open('txt.txt')
 texto=txt.read()
 texto=texto+'\n'
 i=0
-retorno=''
-while(i<len(texto)):
-    if(texto[i]=='\n'):
-        linha=linha+1
-    elif(texto[i]=='{'):
-        while(texto[i]!='}'):
-            i=i+1
-    elif(texto[i] != ' '):
-        if(not(texto[i] in alfabeto) and not(texto[i] in reservado)):
-            se='erro na linha {}: caracter invalido em {}'.format(linha,texto[i])
-            erro.append(se)
-            print(texto[i] + ' - ' + 'erro')
-        else:
-            token=token+texto[i]
-            if((texto[i+1] in separador) or (texto[i+1] in operador) or (texto[i] in separador) or (texto[i] in relacao) or (texto[i] in operador) or (texto[i+1]+texto[i+2] in separador) or (texto[i+1]+texto[i+2] in relacao) or (texto[i+1] in relacao) or (not(texto[i+1] in alfabeto) and not(texto[i+1] in reservado))):
-                if(token in reservado):
-                    if((token in relacao) and (texto[i+1] in relacao)):
-                        i=i+1
-                        token=token+texto[i]
-                    print(token + ' - ' + token)
-                    retorno=token
-                elif(token=='end.'):
-                    print('end - end')
-                    print('. - .')
-                elif(list(filter(token.startswith,num))!= []):
-                    if(not(any(l in [c for c in token] for l in ch))):
-                        if('.' in token):
-                            if(not(token.endswith('.'))):
-                                print(token + ' - ' + 'real')
-                                retorno='numero_real'
+def Analisador_Lexico(i):
+    achou=False
+    retorno=''
+    while(i<len(texto) and not(achou)):
+        if(texto[i]=='\n'):
+            linha=linha+1
+        elif(texto[i]=='{'):
+            while(texto[i]!='}'):
+                i=i+1
+        elif(texto[i] != ' '):
+            if(not(texto[i] in alfabeto) and not(texto[i] in reservado)):
+                se='erro na linha {}: caracter invalido em {}'.format(linha,texto[i])
+                erro.append(se)
+                print(texto[i] + ' - ' + 'erro')
+                achou=True
+            else:
+                token=token+texto[i]
+                if((texto[i+1] in separador) or (texto[i+1] in operador) or (texto[i] in separador) or (texto[i] in relacao) or (texto[i] in operador) or (texto[i+1]+texto[i+2] in separador) or (texto[i+1]+texto[i+2] in relacao) or (texto[i+1] in relacao) or (not(texto[i+1] in alfabeto) and not(texto[i+1] in reservado))):
+                    if(token in reservado):
+                        if((token in relacao) and (texto[i+1] in relacao)):
+                            i=i+1
+                            token=token+texto[i]
+                        print(token + ' - ' + token)
+                        retorno=token
+                    elif(token=='end.'):
+                        print('end - end')
+                        print('. - .')
+                    elif(list(filter(token.startswith,num))!= []):
+                        if(not(any(l in [c for c in token] for l in ch))):
+                            if('.' in token):
+                                if(not(token.endswith('.'))):
+                                    print(token + ' - ' + 'real')
+                                    retorno='numero_real'
+                                else:
+                                    se='erro na linha {}: numero real incompleto em {}'.format(linha,token)
+                                    erro.append(se)
+                                    print(token + ' - ' + 'erro')
                             else:
-                                se='erro na linha {}: numero real incompleto em {}'.format(linha,token)
-                                erro.append(se)
-                                print(token + ' - ' + 'erro')
+                                print(token + ' - ' + 'inteiro')
+                                retorno='numero_int'
                         else:
-                            print(token + ' - ' + 'inteiro')
-                            retorno='numero_int'
+                            se='erro na linha {}: numero mal formado em {}'.format(linha,token)
+                            erro.append(se)
+                            print(token + ' - ' + 'erro')
                     else:
-                        se='erro na linha {}: numero mal formado em {}'.format(linha,token)
-                        erro.append(se)
-                        print(token + ' - ' + 'erro')
-                else:
-                    if(not('.' in token)):
-                        print(token + ' - ' + 'identificador')
-                        retorno='ident'
-                    else:
-                        se='erro na linha {}:identificador mal formado em {}'.format(linha,token)
-                        erro.append(se)
-                        print(token + ' - ' + 'erro')
-            
-                token=''
-    i=i+1
+                        if(not('.' in token)):
+                            print(token + ' - ' + 'identificador')
+                            retorno='ident'
+                        else:
+                            se='erro na linha {}:identificador mal formado em {}'.format(linha,token)
+                            erro.append(se)
+                            print(token + ' - ' + 'erro')
+                
+                    token=''
+                    achou=True
+        i=i+1
+    return(i,retorno)
 print('\nerros: ')
 print(erro)
 
